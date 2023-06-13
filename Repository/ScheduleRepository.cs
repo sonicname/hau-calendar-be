e4e5414c366896daa7903b4ec73c8a68c1e41ed3
yukioqna <nguyenngocdung19082002@gmail.com>
2023-06-14 01:04:25 +0700

    Initial commit

--- Repository/ScheduleRepository.cs
+++ Repository/ScheduleRepository.cs
@@ -1,6 +1,59 @@
-﻿namespace server.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+using server.Models.DTO;
+using System.Collections.Generic;
+
+
+namespace server.Repository;
 
 public class ScheduleRepository
 {
-    
+    private readonly HauCalendarContext _calendarContext;
+    private readonly IConfiguration _configuration;
+
+    public ScheduleRepository(HauCalendarContext context)
+    {
+        _calendarContext = context;
+    }
+
+    public List<ScheduleViewDTO> GetAll(int userId)
+    {
+        var all = (from sbj in _calendarContext.Subjects
+            join scl in _calendarContext.Schedules on sbj.SubjectId equals scl.SubjectId
+            join st in _calendarContext.ScheduleTimes on scl.ScheduleId equals st.ScheduleId
+            where scl.UserId == userId
+            select new ScheduleViewDTO
+            {
+                SubjectName = sbj.SubjectName,
+                SubjectNumCredit = sbj.SubjectNumCredit,
+                ScheduleTimeStarted = scl.ScheduleTimeStarted,
+                ScheduleTimeEnded = scl.ScheduleTimeEnded,
+                DateEnded = st.DateEnded,
+                DateStarted = st.DateStarted,       
+
+            }).ToList();
+                 
+         return all;
+         
+    //     List<ScheduleViewDTO> schedules =  _calendarContext.Subjects
+    //         .Join(
+    //             _calendarContext.Schedules.Join(
+    //                 
+    //                 ),
+    //                 subject => subject.SubjectId,
+    //                 schedule => schedule.SubjectId,
+    //             (subject,schedule) => new {Subject = subject,Schedule=schedule}
+    //             )
+    //         .Join(_calendarContext.ScheduleTimes,
+    //
+    //         )
+    //         .Where()
+    //         .ToList();
+    //    
+    }
+
+    public Delete(int  id)
+    {
+        
+    }
 }
